73b749005ad19c506f26925fbd3a619067c4eeba
sathish16787 <sathish16787@gmail.com>
2025-08-23 14:53:15 +0530

    day 5 learning

---
+++ backend.tf
@@ -0,0 +1,57 @@
+resource "aws_s3_bucket" "random_tf_test" {
+  bucket = "random-tf-test"
+  acl    = "private"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.random_tf_test.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+bucket = aws_s3_bucket.random_tf_test.id
+rule {
+apply_server_side_encryption_by_default {
+sse_algorithm = "AES256"
+}
+}
+}
+
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+bucket = aws_s3_bucket.random_tf_test.id
+block_public_acls = true
+block_public_policy = true
+ignore_public_acls = true
+restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "random-tf-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
+terraform {
+backend "s3" {
+bucket = "random-tf-test"
+key = "global/s3/terraform.tfstate"
+region = "ap-southeast-1"
+dynamodb_table = "random-tf-state-locks"
+encrypt = true
+}
+}
+
+
